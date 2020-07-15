c2b737f32f2e2d5c7555989bddabba37a6f91c0b
Tim <tim241@mailbox.org>
2020-07-15 10:20:59 +0200

    Packer, Unpacker: don't use fixed offsets

--- Packer/Program.cs
+++ Packer/Program.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Packer
 {
     class Program
     {
+        /// <summary>
+        /// Finds byte sequence and returns offset of beginning of byte sequence, returns -1 if not found
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        static long findByteSequence(FileStream stream, byte[] bytes)
+        {
+            byte[] buffer = new byte[1024];
+            byte[] tmpBuffer = new byte[bytes.Length];
+            int tmpBufferIndex = 0;
+            int readCount;
+
+            while ((readCount = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                for (int i = 0; i < readCount; i++)
+                {
+                    if (buffer[i] == bytes[tmpBufferIndex])
+                    {
+                        tmpBufferIndex++;
+                        tmpBuffer[tmpBufferIndex] = buffer[i];
+
+                        // found the bytes, return offset
+                        if (tmpBufferIndex == (bytes.Length - 1))
+                            return (stream.Position - buffer.Length - bytes.Length + i + 2);
+                    }
+                    else
+                    {
+                        tmpBufferIndex = 0;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
         static void Main(string[] args)
         {
             // packed executable starts at 0x1A71A2
@@ -16,31 +53,44 @@ namespace Packer
                 Environment.Exit(1);
             }
 
-            string gameExecutable = args[0];
-            string injectExecutable = args[1];
+            FileStream writer = File.Open(args[0], FileMode.Open);
+            FileStream reader = File.OpenRead(args[1]);
 
-            if (new FileInfo(injectExecutable).Length > (0x50BB21 - 0x1A71A2))
-            {
-                Console.WriteLine("executable too big to inject!");
-                Environment.Exit(1);
-            }
 
-            BinaryWriter gameExecutableStream = new BinaryWriter(File.OpenWrite(gameExecutable));
-            byte[] injectExecutableBytes = File.ReadAllBytes(injectExecutable);
+            // find required byte sequences
+            byte[] drmStartBytes = Encoding.ASCII.GetBytes("!popcapdrmprotect!");
+            byte[] drmEndBytes = Encoding.ASCII.GetBytes("!popcapdrmprotend!");
 
-            // set starting position
-            gameExecutableStream.BaseStream.Position = 0x1A71A2;
+            long drmStartOffset = findByteSequence(writer, drmStartBytes);
+            long drmEndOffset = findByteSequence(writer, drmEndBytes);
 
-            // write executable into stream
-            gameExecutableStream.Write(injectExecutableBytes);
+            if (drmStartOffset == -1 || drmEndOffset == -1)
+                throw new Exception("Required byte sequence not found!");
 
-            // fill with 0 until we've reached the end
-            while (gameExecutableStream.BaseStream.Position <= 0x50BB21)
-                gameExecutableStream.Write(new byte[] { 0 });
+            writer.Position = drmStartOffset + drmStartBytes.Length;
 
-            // flush & close stream
-            gameExecutableStream.Flush();
-            gameExecutableStream.Close();
+            // the game needs 400 zero bytes after the drmStartBytes offset for some reason,
+            // so let's respect that
+            writer.Position += 400;
+
+            if (reader.Length > (drmEndOffset - writer.Position))
+                throw new Exception("Executable too big to inject!");
+
+            // write executable into the game executable
+            byte[] buffer = new byte[1024];
+            int bufferCount;
+            while ((bufferCount = reader.Read(buffer, 0, buffer.Length)) > 0)
+                writer.Write(buffer, 0, bufferCount);
+
+            // fill rest with 0 bytes until we've reached the end
+            buffer = new byte[] { 0 };
+            while (writer.Position < drmEndOffset)
+                writer.Write(buffer, 0, buffer.Length);
+
+            // flush & close
+            writer.Flush();
+            writer.Close();
+            reader.Close();
         }
     }
 }
--- Unpacker/Program.cs
+++ Unpacker/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Unpacker
 {
     class Program
     {
+        /// <summary>
+        /// Finds byte sequence and returns offset of beginning of byte sequence, returns -1 if not found
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        static long findByteSequence(BinaryReader stream, byte[] bytes)
+        {
+            byte[] buffer = new byte[1024];
+            byte[] tmpBuffer = new byte[bytes.Length];
+            int tmpBufferIndex = 0;
+            int readCount;
+
+            while ((readCount = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                for (int i = 0; i < readCount; i++)
+                {
+                    if (buffer[i] == bytes[tmpBufferIndex])
+                    {
+                        tmpBufferIndex++;
+                        tmpBuffer[tmpBufferIndex] = buffer[i];
+
+                        // found the bytes, return offset
+                        if (tmpBufferIndex == (bytes.Length - 1))
+                            return (stream.BaseStream.Position - buffer.Length - bytes.Length + i + 2);
+                    }
+                    else
+                    {
+                        tmpBufferIndex = 0;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
         static void Main(string[] args)
         {
-            // packed executable starts at 0x1A71A2
-            // packed executable ends at   0x50BB22
-
             if (args.Length < 2)
             {
                 Console.WriteLine("Usage: Unpacker.exe [PlantsVsZombies.exe] [output file]");
@@ -19,20 +55,35 @@ namespace Unpacker
             BinaryReader reader = new BinaryReader(File.OpenRead(args[0]));
             BinaryWriter writer = new BinaryWriter(File.OpenWrite(args[1]));
 
-            reader.BaseStream.Position = 0x1A71A2;
+            // find required byte sequences
+            byte[] drmStartBytes = Encoding.ASCII.GetBytes("!popcapdrmprotect!");
+            byte[] drmEndBytes = Encoding.ASCII.GetBytes("!popcapdrmprotend!");
 
+            long drmStartOffset = findByteSequence(reader, drmStartBytes);
+            long drmEndOffset = findByteSequence(reader, drmEndBytes);
+
+            if (drmStartOffset == -1 || drmEndOffset == -1)
+                throw new Exception("Required byte sequence not found!");
+
+            reader.BaseStream.Position = drmStartOffset + drmStartBytes.Length;
+
+            // skip first few useless 0 bytes (why do they exist at all?)
+            // until we get something that isn't a 0 byte
+            while (reader.ReadByte() == 0)
+                continue;
+            reader.BaseStream.Position--;
+
+            // read until drmEndOffset
             byte[] buffer = new byte[1];
-
-            // read until 0x50BB22
             while (reader.Read(buffer, 0, 1) > 0)
             {
                 writer.Write(buffer);
 
-                if (reader.BaseStream.Position == 0x50BB21)
+                if (reader.BaseStream.Position == drmEndOffset)
                     break;
             }
 
-            // close & flush
+            // flush & close
             writer.Flush();
             writer.Close();
             reader.Close();
